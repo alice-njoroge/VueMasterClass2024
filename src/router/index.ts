9438ea103c95faf17c385bbddee7a1366c1117ff
Alice Wanjiku <alicenjoroge707@gmail.com>
2024-06-19 20:22:24 +0200

    introducing file based routing  with unplugin-vue-router plugin

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router/auto';
+import {routes} from 'vue-router/auto-routes'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    { path: '/', name: 'home', component: () => import('@/views/HomeView.vue') },
-    { path: '/projects', name: 'projects', component: () => import('@/views/ProjectView.vue') },
-    { path: '/projects/:id(\\d+)', name: 'project', component: () => import('@/views/singleProject.vue') }, //dynamic route matching with params
-    { path: '/projects:pathMatch(.*)*', name: 'notFound', component: () => import('@/views/NotFound.vue') }
-  ]
+  routes
 })
 
 export default router
