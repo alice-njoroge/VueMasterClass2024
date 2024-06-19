34031ffb82d123361793745968dd329b861d5376
Alice Wanjiku <alicenjoroge707@gmail.com>
2024-06-19 12:57:02 +0200

    Regex to only numeric params and 404 page

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,8 +4,9 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', name: 'home', component: () => import('@/views/HomeView.vue') },
-    { path: '/projects', name: 'projects', component: () => import('@/views/ProjectView.vue')},
-    {path: '/projects/:id', name:'project', component: () => import('@/views/singleProject.vue')} //dynamic route matching with params
+    { path: '/projects', name: 'projects', component: () => import('@/views/ProjectView.vue') },
+    { path: '/projects/:id(\\d+)', name: 'project', component: () => import('@/views/singleProject.vue') }, //dynamic route matching with params
+    { path: '/:pathMatch(.*)*', name: 'notFound', component: () => import('@/views/NotFound.vue') }
   ]
 })
 
---
+++ src/views/NotFound.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <h2> Ooops! Page not found, go back <router-link to="/"> Home</router-link>
+  </h2>
+</template>
+
+<style scoped>
+
+</style>
