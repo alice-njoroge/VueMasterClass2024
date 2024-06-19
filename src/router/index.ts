288916e291635a40723565b8d16254c8710f3560
Alice Wanjiku <alicenjoroge707@gmail.com>
2024-06-19 12:31:55 +0200

    Dynamic route matching with params

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
-import ProjectView from '@/views/ProjectView.vue'
-
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {path: '/', name:'home', component: HomeView},
-    {path: '/projects', name:'projects', component: ProjectView},
+    { path: '/', name: 'home', component: () => import('@/views/HomeView.vue') },
+    { path: '/projects', name: 'projects', component: () => import('@/views/ProjectView.vue')},
+    {path: '/projects/:id', name:'project', component: () => import('@/views/singleProject.vue')} //dynamic route matching with params
   ]
 })
 
---
+++ src/views/singleProject.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import {useRoute} from 'vue-router';
+
+const route = useRoute();
+
+</script>
+
+<template>
+<h2> Project {{route.params.id}} </h2>
+</template>
+
+<style scoped>
+
+</style>
